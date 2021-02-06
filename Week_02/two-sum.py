29d8b9267cef43a6bdaa7a67ad1b225e53574e9b
slowisfaster <51299816@qq.com>
2021-02-06 23:51:32 +0800

    homework

---
+++ Week_02/two-sum.py
@@ -0,0 +1,16 @@
+#暴力
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums) - 1):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
+#哈希
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashtable = dict()
+        for i, num in enumerate(nums):
+            if target - num in hashtable:
+                return [hashtable[target - num], i]
+            hashtable[nums[i]] = i
+        return []
