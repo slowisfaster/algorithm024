c283b85264e4bf76c36d249ac6ef39e360d0ed66
slowisfaster <51299816@qq.com>
2021-03-22 00:08:30 +0800

    week07

---
+++ Week_07/climbing-stairs.py
@@ -0,0 +1,19 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        #if n<=2: return n
+        #return self.climbStairs(n-1) + self.climbStairs(n-2)
+        s = {}
+        s[0] = 1
+        s[1] = 2
+        for i in range(2, n):
+            s[i] = s[i-1] + s[i-2]
+        return s[n-1]
+        """
+        if n<=2: return n
+        a, b, res = 1, 2, 0
+        for i in range(2, n):
+            res = a + b
+            a = b
+            b = res
+        return res
+        """
